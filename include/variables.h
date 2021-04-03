f98b75d7b7ca6110669e47ee9d34a03235b7bba5
Richard Nader, Jr <rmnader@svsu.edu>
2021-04-02 22:22:38 -0400

    Changed pin assignments for LITE proto-boards.

--- include/variables.h
+++ include/variables.h
@@ -3,20 +3,20 @@
 // ----- Static Controller Settings -----
 
 // Settings for led_control.h
-#define RGB_LED_PIN 12
+#define RGB_LED_PIN A1
 #define LED_DIM_DELAY 10000
 #define LED_DIM_SPEED 5000 // Dim led in 5 seconds
 
 
 // Settings for motor_control.h
-#define STEP_PIN 2
-#define DIR_PIN 3
-#define ENABLE_PIN 4
-#define MICRO_PIN_1 5
-#define MICRO_PIN_2 6
-#define MICRO_PIN_3 7
-#define OPEN_ENDSTOP_PIN 8
-#define CLOSE_ENDSTOP_PIN 9
+#define STEP_PIN 3
+#define DIR_PIN 2
+#define ENABLE_PIN 9
+#define MICRO_PIN_1 8
+#define MICRO_PIN_2 7
+#define MICRO_PIN_3 6
+#define OPEN_ENDSTOP_PIN 11
+#define CLOSE_ENDSTOP_PIN 12
 
 #define MAX_MOTOR_SPEED 10000
 #define MOTOR_SPEED 9500
@@ -24,8 +24,8 @@
 #define MOTOR_RUN_TIMEOUT 15000 // Motor should not run for more than 15 seconds
 
 // Settings for remote_control.h
-#define MANUAL_OPEN_BUTTON 10
-#define MANUAL_CLOSE_BUTTON 11
+#define MANUAL_OPEN_BUTTON A4
+#define MANUAL_CLOSE_BUTTON A5
 
 // ----- END Static Controller Settings -----
 
